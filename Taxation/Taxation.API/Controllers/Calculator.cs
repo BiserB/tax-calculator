37392b9e158a6bf264f445dc66e2d8b82ffa3e9b
Biser Borisov <butoff@abv.bg>
2024-06-26 08:32:05 +0300

    Add an initial project structure

---
+++ Taxation/Taxation.API/Controllers/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Taxation.Domain.Constants;
+
+namespace Taxation.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Calculator : ControllerBase
+    {
+        [HttpGet(Name = "GetLegislation")]
+        public string Get()
+        {
+            return Legislation.Content;
+        }
+    }
+}
